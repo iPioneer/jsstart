9fab2d8898679fb579cc2d801f6a6ce863a2558d
Evgeny <evgenysoloshenko@gmail.com>
2019-08-08 21:14:10 +0300

    First commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+let persone = {
+    name:"John",
+    age:"25",
+    isMaried:"true"
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+for (let i = 0; i < 3; i++){
+console.log(arr[i]);
+}
+let answer = prompt("You are there!", "Yeah");
+console.log(typeof(null));
